6d70e9e1380875adff5bd7a22a8cce235a4b0d4f
George Hotz <geohot@gmail.com>
2019-11-03 11:29:59 -0800

    add scoping

--- metamath/verify.py
+++ metamath/verify.py
@@ -6,11 +6,26 @@ from lark import Lark
 l = Lark(open("mm.g").read())
 p = l.parse(open("miu2.mm" if len(sys.argv)==1 else sys.argv[1]).read())
 
-constants = set()
-asserts = dict()
-hypos = dict()
-variables = dict()
-essen = dict()
+class Scope(object):
+  def __init__(self):
+    self.constants = set()
+    self.asserts = dict()
+    self.hypos = dict()
+    self.variables = dict()
+    self.essen = dict()
+
+  def child(self):
+    ret = Scope()
+    # asserts are noscope
+    ret.asserts = self.asserts
+
+    ret.constants = self.constants.copy()
+    ret.variables = self.variables.copy()
+
+    ret.hypos = self.hypos.copy()
+    ret.essen = self.essen.copy()
+
+    return ret
 
 def lp(ms):
   return ' '.join(map(str, ms))
@@ -31,7 +46,7 @@ class Stack(object):
     print("*** popped", ret[0], lp(ret[1]))
     return ret
 
-def verify_proof(intyc, inms, xx):
+def verify_proof(scope, intyc, inms, xx):
   xx = xx.children[0]
   stack = Stack()
   for s in xx.children:
@@ -40,22 +55,23 @@ def verify_proof(intyc, inms, xx):
       # then bind in normal scope
       nms = []
       for v in ms[::-1]:
-        if v in variables:
+        if v in scope.variables:
           if v not in bindings:
             vt, vnms = stack.pop()
-            assert variables[v]['type'] == vt
+            assert scope.variables[v]['type'] == vt
             bindings[v] = vnms
           nms.append(bindings[v])
         else:
           # pass through constants
+          assert v in scope.constants
           nms.append([v])
       ret = []
       for x in nms[::-1]:
         ret += x
       return ret
 
-    if s in asserts:
-      a = asserts[s]
+    if s in scope.asserts:
+      a = scope.asserts[s]
       ms = a['ms']
       # first bind in essential scope
       for e in a['essen'].values():
@@ -68,10 +84,13 @@ def verify_proof(intyc, inms, xx):
 
       nms = bind(ms)
       stack.push(a['type'], nms)
-    elif s in hypos:
-      a = hypos[s]
+    elif s in scope.hypos:
+      a = scope.hypos[s]
       # don't bind variables
       stack.push(a['type'], a['ms'])
+    else:
+      raise Exception("%s label not found" % s)
+
 
   # confirm stack is this
   o = stack.pop()
@@ -79,69 +98,61 @@ def verify_proof(intyc, inms, xx):
   assert(len(stack) == 0)
   assert o == (intyc, inms)
 
-def parse_stmt(xx):
+def parse_stmt(scope, xx):
   if xx.data == "variable_stmt":
     for y in xx.children:
       vname = y.children[0]
-      assert vname not in variables
-      variables[vname] = {}
+      assert vname not in scope.variables
+      scope.variables[vname] = {}
   elif xx.data == "hypothesis_stmt":
     xx = xx.children[0]
     lbl = xx.children[0]
     tyc = xx.children[1].children[0].children[0]
-    assert tyc in constants
+    assert tyc in scope.constants
     if xx.data == "floating_stmt":
       var = xx.children[2].children[0]
-      assert var in variables
+      assert var in scope.variables
       # TODO: we are throwing away this name, do we need it?
-      variables[var]['type'] = tyc
-      hypos[lbl] = {"type": tyc, "ms": [var]}
-      #code.interact(local=locals())
-      #pass
+      scope.variables[var]['type'] = tyc
+      scope.hypos[lbl] = {"type": tyc, "ms": [var]}
     elif xx.data == "essential_stmt":
       ms = xx.children[2:]
-      essen[lbl] = {"type": tyc, "ms": ms}
+      scope.essen[lbl] = {"type": tyc, "ms": ms}
+      scope.hypos[lbl] = {"type": tyc, "ms": ms}
   elif xx.data == "assert_stmt":
     xx = xx.children[0]
     lbl = xx.children[0]
     tyc = xx.children[1].children[0].children[0]
-    assert tyc in constants
+    assert tyc in scope.constants
     if xx.data == "axiom_stmt":
       ms = xx.children[2:]
-      asserts[lbl] = {'type': tyc, 'ms': ms, 'essen': essen.copy()}
     elif xx.data == "provable_stmt":
       ms = xx.children[2:-1]
       proof = xx.children[-1]
       print("verifying proof for %s" % lbl)
-      verify_proof(tyc, ms, proof)
-      asserts[lbl] = {'type': tyc, 'ms': ms}
+      verify_proof(scope, tyc, ms, proof)
       print("verified proof for %s" % lbl)
-    #print(asserts[lbl])
-    #print(xx.pretty())
+    scope.asserts[lbl] = {'type': tyc, 'ms': ms, 'essen': scope.essen.copy()}
   else:
-    #print(xx.pretty())
-    print(xx.data)
+    print("IMPLEMENT", xx.data)
     pass
 
-
+scope = Scope()
 for x in p.children:
   xx = x.children[0]
   if xx.data == "constant_stmt":
     for y in xx.children:
       cname = y.children[0]
-      assert cname not in constants
-      constants.add(cname)
+      assert cname not in scope.constants
+      scope.constants.add(cname)
   else:
     xx = xx.children[0]
     if xx.data == "block":
-      # TODO: scoping
+      tscope = scope.child()
       for y in xx.children:
-        parse_stmt(y.children[0])
-      # reset essential statements
-      # TODO: other scoping
-      essen = dict()
+        parse_stmt(tscope, y.children[0])
     else:
-      parse_stmt(xx)
+      parse_stmt(scope, xx)
 
   #print(x.children[0].pretty())
 
