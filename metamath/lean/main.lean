cf775eeb727a4856de55762a03dce2bd6b23df33
George Hotz <geohot@gmail.com>
2019-11-24 14:45:18 -0800

    parse everything

--- metamath/lean/main.lean
+++ metamath/lean/main.lean
@@ -29,10 +29,22 @@ def consume_until (s : string) : list string -> (list string) × (list string)
 | [] := ([], [])
 | (a :: l) := if a ≠ s then let (aa, bb) := consume_until l in (a :: aa, bb) else ([], l)
 
-def parser : list string -> list stmt
-| ("$c" :: l) := let (consts, rest) := consume_until "$." l in [stmt.constant_stmt consts]
-| ("$v" :: l) := [stmt.variable_stmt l]
-| default := [stmt.parse_error]
+def parser_core : nat -> list string -> list stmt
+| 0 := λ x, [stmt.parse_error]
+| (n+1) := let pc := (parser_core n) in λ x, match x with
+    | ("$c" :: l) := let (x, rest) := consume_until "$." l in (stmt.constant_stmt x) :: (pc rest)
+    | ("$v" :: l) := let (x, rest) := consume_until "$." l in (stmt.variable_stmt x) :: (pc rest)
+    | (name :: "$f" :: typecode :: var :: "$." :: l) := (stmt.floating_stmt name typecode var) :: (pc l)
+    | (name :: "$a" :: typecode :: l) := let (x, rest) := consume_until "$." l in
+        (stmt.axiom_stmt name typecode x) :: (pc rest) 
+    | (name :: "$p" :: typecode :: l) := let (x, rest) := consume_until "$=" l in
+        let (y, rest2) := consume_until "$." rest in
+            (stmt.provable_stmt name typecode x y) :: (pc rest2) 
+    | [] := []
+    | default := [stmt.parse_error]
+    end
+
+def parser (s : list string) : list stmt := ((parser_core s.length) s)
 
 def whitespace : char -> bool
 | ' ' := true
@@ -53,7 +65,7 @@ do a <- get_file,
    let s := a.to_string,
    let l := lexer s,
    let p := parser l,
-   put_str ((list.to_string p) ++ "\n")
+   put_str ((p.foldl (λ r s, r ++ "\n" ++ (stmt_to_string s)) "PARSE LIST") ++ "\n")
    --put_str ((list.to_string l) ++ "\n" ++ (list.to_string p) ++ "\n")
 
 #eval main
