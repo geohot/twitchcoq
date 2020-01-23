6a82031adadef8eaa982344ba67e6e4e803008d4
George Hotz <geohot@gmail.com>
2020-01-23 12:04:08 -0800

    prop logic = sat solving

---
+++ harrison/chap2.ml
@@ -0,0 +1,12 @@
+type ('a)formula = False
+                 | True
+                 | Atom of 'a
+                 | Not of ('a)formula
+                 | And of ('a)formula * ('a)formula
+                 | Or of ('a)formula * ('a)formula
+                 | Imp of ('a)formula * ('a)formula
+                 | Iff of ('a)formula * ('a)formula
+                 | Forall of string * ('a)formula
+                 | Exists of string * ('a)formula;;
+
+
