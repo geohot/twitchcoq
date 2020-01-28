bd3f0756b0edd1ee4ddb49c335c7837480d068dc
George Hotz <geohot@gmail.com>
2020-01-27 17:28:38 -0800

    tnf poorly enums

--- bb/run.py
+++ bb/run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # 6 possible states
 # 2 values
@@ -8,32 +8,38 @@
 
 # See Tree Normal Form from Bra66
 
-t0 = "B1L C1R A1L A1L H1L".split(" ")
-t1 = "A1L B1R D1R E1R C0R".split(" ")
+t0 = "B1L A1L C1R B1R A1L D1R A1L E1R H1L C0R".split(" ")
 
-t = [0]*30000
-pos = 15000
-s = "A"
+def run(t0, ms=-1):
+  t = [0]*30000
+  pos = 15000
+  s = "A"
 
-cnt = 0
-while 1:
-  assert pos > 0
-  assert pos < len(t)
-  if cnt%1000 == 0:
-    print(cnt, s, pos)
-  ns = ord(s) - ord("A")
-  if t[pos] == 0:
-    tt = t0[ns]
-  elif t[pos] == 1:
-    tt = t1[ns]
-  s = tt[0]
-  t[pos] = int(tt[1])
-  if tt[2] == "L":
-    pos -= 1
-  else:
-    pos += 1
-  if s == "H":
-    break
-  cnt += 1
+  cnt = 0
+  halt = True
+  while 1:
+    assert pos > 0
+    assert pos < len(t)
+    #if cnt%1000 == 0:
+    #  print(cnt, s, pos)
+    ns = ord(s) - ord("A")
+    if t[pos] == 0:
+      tt = t0[ns*2+0]
+    elif t[pos] == 1:
+      tt = t0[ns*2+1]
+    s = tt[0]
+    t[pos] = int(tt[1])
+    if tt[2] == "L":
+      pos -= 1
+    else:
+      pos += 1
+    if s == "H":
+      break
+    cnt += 1
+    if ms != -1 and cnt > ms:
+      halt = False
+      break
+  return halt, cnt, sum(t)
 
-print(sum(t))
+if __name__ == "__main__":
+  print(run(t0))
---
+++ bb/tnf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+# https://sci-hub.tw/10.1109/PGEC.1966.264572
+# Hmm, the tree normal form description is kind of annoying, it's a graph, right?
+# Oh, "Attacking the Busy Beaver 5" has a clearer description
+# Does the first move need to print a one?
+
+# A0 = B1L looks like the default start in "Attacking the Busy Beaver 5"
+# E0 | E1 = H1L is the final one
+
+# They even seem to be reasoning about the machines while they generate them
+# This is a little annoying as the function can only be computed in order
+
+# Hmm https://arxiv.org/pdf/1610.03184.pdf
+
+from run import run
+
+# 4*2*2 = 16
+# 16^6 possible for sigma(3, 2)
+
+def gen(x,b=3):
+  e = (b+1)*2*2
+  ret = []
+  for i in range(b*2):
+    t = x%e
+    ss = "L" if t%2 else "R"
+    t //= 2
+    ss = ("0" if t%2 else "1") + ss
+    t //= 2
+    if t==b:
+      ss = "H" + ss
+    else:
+      ss = chr(ord("A") + t) + ss
+    ret.append(ss)
+    x //= e
+  return ret
+
+if __name__ == "__main__":
+  b = 2
+  all_s = 0
+  all_sigma = 0
+  for m in range(((b+1)*2*2)**(b*2)):
+    mm = gen(m, b)
+    halt, s, sigma = run(mm, ms=100)
+    print(mm, halt, s, sigma, all_s, all_sigma)
+    if halt:
+      all_s = max(s, all_s)
+      all_sigma = max(sigma, all_sigma)
+
+
+
+
